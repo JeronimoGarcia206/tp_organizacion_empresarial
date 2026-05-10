36ede814e6b2d368e71e26ccd68821f967096c70
JeronimoGarcia206 <Jeronimogarcia206@gmail.com>
2026-05-10 06:42:45 +0000

    PROY-2: Desarrollo del análisis de ventas

--- scripts/analisis_ventas.py
+++ scripts/analisis_ventas.py
@@ -1,37 +1,22 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 
-# Lectura del dataset
+#Lectura del dataset
 df = pd.read_csv("datos/ventas.csv")
 
-# Calcular total por venta
+#Calcular total por venta
 df["total"] = df["cantidad"] * df["precio"]
 
-# Ventas totales
+#Ventas totales
 ventas_totales = df["total"].sum()
 
-# Producto más vendido
-ventas_producto = df.groupby("producto")["cantidad"].sum()
-producto_mas_vendido = ventas_producto.idxmax()
+#Producto más vendido
+producto_mas_vendido = df.groupby("producto")["cantidad"].sum().idxmax()
 
-# Mostrar resultados
+#Mostrar resultados
 print("Ventas totales:", ventas_totales)
 print("Producto más vendido:", producto_mas_vendido)
 
-# Guardar resumen
+#Guardar resultados
 with open("resultados/resumen.txt", "w") as archivo:
     archivo.write(f"Ventas totales: {ventas_totales}\n")
     archivo.write(f"Producto más vendido: {producto_mas_vendido}")
-
-# Generar gráfico
-ventas_producto.plot(kind="bar")
-
-plt.title("Cantidad vendida por producto")
-plt.xlabel("Producto")
-plt.ylabel("Cantidad")
-
-# Guardar gráfico
-plt.savefig("resultados/grafico_ventas.png")
-
-# Mostrar gráfico
-plt.show()
